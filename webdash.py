f95ebaac373eee62116f862e0bf22a54720b8980
nzimm <nizi2734@colorado.edu>
2020-05-09 17:09:47 -0600

    added websockets to auto-refresh on update

--- webdash.py
+++ webdash.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from flask import *
+from flask_socketio import SocketIO, send, emit
 import util
 
 app = Flask(__name__, template_folder="./views/")
+socketio = SocketIO(app)
 
 # store links for the dashboard
 # could look at redis for persistence
@@ -14,14 +16,16 @@ context = {
 def index():
     return render_template("base.html", url=context["display_url"])
 
-
 @app.route("/update", methods=["POST"])
 def update_link():
     if not "url" in request.form:
         return Response("error", 400)
     context["display_url"] = request.form["url"]
+
+    # emit socket event to tell client to reload dashboard
+    emit('reload', namespace="/", broadcast=True)
+
     return jsonify("updated display to {}".format(request.form["url"]))
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
